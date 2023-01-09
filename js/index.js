d0decd4f21487feab10b42ccf84a1176f41c35ec
D Binod <dbinod2020@gmail.com>
2023-01-09 21:41:16 +0530

    Everything is done rather than make responsive.

--- js/index.js
+++ js/index.js
@@ -3,13 +3,41 @@
 
 let change_theme_btn= document.querySelector("#change-theme>.theme-logo");
 
+
 change_theme_btn.onclick=function(){
     document.body.classList.toggle("dark-mode");
     if(document.body.classList.contains("dark-mode")){
+        // ....changing the icon..
         change_theme_btn.innerHTML='<i class="fa-solid fa-sun"></i>';
+
+        //change the github caleander to dark.
+        document.querySelector("#github-cal img").src="../img/github-cal(dark).png"
+
+        //change the github streak to dark.
+        document.querySelector("#github-streak-stat>#github-streak img").src="https://github-readme-streak-stats.herokuapp.com/?user=binodgith&show_icons=true&locale=en&theme=tokyonight"
+
+        //change the github stat to dark
+        document.querySelector("#github-streak-stat>#github-stat img").src="https://github-readme-stats.vercel.app/api?username=binodgith&show_icons=true&locale=en&theme=tokyonight"
+
+        // change the github langusge to dark
+        document.querySelector("#github-lang img").src="https://github-readme-stats.vercel.app/api/top-langs?username=tusharkadam7671&show_icons=true&locale=en&layout=compact&theme=tokyonight"
+
     }
     else{
         change_theme_btn.innerHTML='<i class="fa-solid fa-moon"></i>';
+
+
+        //change the github caleander to dark.
+        document.querySelector("#github-cal img").src="../img/github-cal(light).png"
+
+        //change the github streak to dark.
+        document.querySelector("#github-streak-stat>#github-streak img").src="https://github-readme-streak-stats.herokuapp.com/?user=binodgith&show_icons=true&locale=en&"
+
+        //change the github stat to dark
+        document.querySelector("#github-streak-stat>#github-stat img").src="https://github-readme-stats.vercel.app/api?username=binodgith&show_icons=true&locale=en&"
+
+        // change the github langusge to dark
+        document.querySelector("#github-lang img").src="https://github-readme-stats.vercel.app/api/top-langs?username=tusharkadam7671&show_icons=true&locale=en&layout=compact&"
     }
 }
 
@@ -53,4 +81,32 @@ document.querySelector("#social-icon-1>.mail-icon").addEventListener("click",fun
 
 
 
+// upload inuput visibility ...............
+
+
+let upload_input=document.querySelector("#upload-box");
+
+document.querySelector("#ask-upload").addEventListener("change",function(){
+    let ask_upload=document.querySelector("#ask-upload").value;
+
+    if(ask_upload=="true"){
+        upload_input.style.visibility="visible";
+        upload_input.style.position="relative";
+    
+    }
+    else{
+        upload_input.style.visibility="hidden";
+        upload_input.style.position="absolute";
+    }
+})
+
+
+
+
+
+//current Year appending in footer section.
+
+let date=new Date();
+let year=date.getFullYear();
+document.getElementById("footer-year").innerText=year;
 
